fbc04826bccba96312ce52fdfc8aa128ec94befb
Chamu Mutezva <ckmutezva@gmail.com>
2024-08-25 07:08:08 +0200

    vercel image optimization - reduce traffic

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'image.tmdb.org',
-            port: '',
-            pathname: '/t/p/original/**',
-          },
+            {
+                protocol: "https",
+                hostname: "image.tmdb.org",
+                port: "",
+                pathname: "/t/p/original/**",
+            },
         ],
-      },
-}
+    },
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
